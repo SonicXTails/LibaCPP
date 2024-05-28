4be1a12bc0aaeec5a57ebc6c08bf565787644d80
SonicXTails <aedgy.master@mail.ru>
2024-05-28 16:46:04 +0300

    ---

---
+++ WorkWithDynamicLiba/Liba/Liba.cpp
@@ -0,0 +1,39 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <C:\WorkStringLiba\WorkWithDynamicLiba\Liba\Header.h>
+
+typedef bool (*ContainsAllCharsFunc)(const char*, const char*);
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    std::string inputString;
+    std::string searchString;
+
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, inputString);
+
+    std::cout << "Введите символы для поиска: ";
+    std::getline(std::cin, searchString);
+
+    HINSTANCE load;
+    load = LoadLibrary(L"WorkWithDynamicLiba.dll");
+    if (load != NULL) {
+        ContainsAllCharsFunc ContainsAllChars = (ContainsAllCharsFunc)GetProcAddress(load, "ContainsAllChars");
+
+        if (ContainsAllChars != NULL) {
+            bool result = ContainsAllChars(inputString.c_str(), searchString.c_str());
+            std::cout << "Результат: " << (result ? "Все символы найдены." : "Не все символы найдены.") << std::endl;
+        }
+        else {
+            std::cerr << "Функция не найдена." << std::endl;
+        }
+
+        FreeLibrary(load);
+    }
+    else {
+        std::cerr << "DLL не найдена." << std::endl;
+    }
+
+    return 0;
+}
---
+++ WorkWithDynamicLiba/WorkWithDynamicLiba/WorkWithDynamicLiba.cpp
@@ -0,0 +1,13 @@
+﻿#include <windows.h>
+#include <string>
+
+extern "C" __declspec(dllexport) bool ContainsAllChars(const char* str, const char* chars) {
+    std::string s(str);
+    std::string c(chars);
+    for (char ch : c) {
+        if (s.find(ch) == std::string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
